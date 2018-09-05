324a4646628438f83a75aa04aaa3578d77f24a8f
dante <c.dante.federici@gmail.com>
2018-09-04 23:39:22 -0400

    Moving to webpack 4

--- webpack.config.js
+++ webpack.config.js
@@ -2,38 +2,43 @@
 
 const webpack = require('webpack');
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
+	entry: './src/index.js',
+	devtool: 'inline-source-map',
+	devServer: {
+		contentBase: './build',
+	},
 
-    entry: './src/index.js',
+	plugins: [
+		new CleanWebpackPlugin(['build']),
+		new HtmlWebpackPlugin({
+			title: 'over-there',
+		}),
+		new webpack.DefinePlugin({
+			'CANVAS_RENDERER': JSON.stringify(true),
+			'WEBGL_RENDERER': JSON.stringify(true)
+		})
+	],
 
-		devtool: 'eval-source-map',
+	output: {
+		path: path.resolve(__dirname, 'build'),
+		filename: '[contenthash].bundle.js'
+	},
 
-    output: {
-        path: path.resolve(__dirname, 'build'),
-        publicPath: '/build/',
-        filename: 'project.bundle.js'
-    },
-
-    module: {
-        rules: [
-          {
-            test: [ /\.vert$/, /\.frag$/ ],
-            use: 'raw-loader'
-          },
-					{
-						test: /\.js$/,
-						exclude: /node_modules/,
-          	use: ['babel-loader'],
-        	}
-        ]
-    },
-
-    plugins: [
-        new webpack.DefinePlugin({
-            'CANVAS_RENDERER': JSON.stringify(true),
-            'WEBGL_RENDERER': JSON.stringify(true)
-        })
-    ]
-
+	module: {
+		rules: [
+			{
+				test: [ /\.vert$/, /\.frag$/ ],
+				use: 'raw-loader'
+			},
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: ['babel-loader'],
+			}
+		]
+	},
 };
